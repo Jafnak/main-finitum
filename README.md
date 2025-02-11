9c0156384d28761a459d4875280c98e867068b91
Jafnak <jafnakusman@gmail.com>
2025-02-11 22:02:31 +0530

    doc: add pages and technologies to README

---
+++ README.md
@@ -0,0 +1,14 @@
+## Pages
+- login
+- signup
+- homepage
+- sessions:
+  - listing
+  - add session
+  - session show page
+
+## Technologies
+- React 
+- Node JS/Express JS
+- MongoDB
+  
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Card from "./Components/Card";
+
+export default function App() {
+return (
+    <div>
+<Card content='card content1' title='card title1'/>
+<Card content='card content2' title='card title2'/>
+
+</div>
+);
+}
+  
+
+

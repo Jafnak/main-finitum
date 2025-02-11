dfd324bc967a46abab7c9ef9ab24b3521264aeee
Jafnak <jafnakusman@gmail.com>
2025-02-11 22:32:02 +0530

    fix: remove missing card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
-import Card from "./Components/Card";
-
 export default function App() {
-return (
+  return (
     <div>
-<Card content='card content1' title='card title1'/>
-<Card content='card content2' title='card title2'/>
-
-</div>
-);
+      <h1>Hello world</h1>
+    </div>
+  );
 }
-  
-
-
